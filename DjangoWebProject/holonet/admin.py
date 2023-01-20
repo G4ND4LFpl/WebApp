10b7df0d30991675f9e8f7ab20b1f32939a595fb
G4ND4LFpl <71659952+G4ND4LFpl@users.noreply.github.com>
2023-01-20 13:45:58 +0100

    Holonet model

--- DjangoWebProject/holonet/admin.py
+++ DjangoWebProject/holonet/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User,Post
+from .models import Post, HolonetUser
 
-admin.site.register(User)
 admin.site.register(Post)
+admin.site.register(HolonetUser)
--- DjangoWebProject/holonet/models.py
+++ DjangoWebProject/holonet/models.py
@@ -1,17 +1,14 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    user_name = models.CharField(unique=True,max_length=20)
-    join_date = models.DateTimeField(default=datetime.now)
-    password = models.CharField(max_length=60)
-
-    def __str__(self):
-        return self.user_name
+class HolonetUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.TextField()
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(HolonetUser, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField(default=datetime.now)
     likes = models.IntegerField(default=0)
