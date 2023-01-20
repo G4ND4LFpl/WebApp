66ad18bb7d9c07e64288dad872c89a88b7a69f15
G4ND4LFpl <71659952+G4ND4LFpl@users.noreply.github.com>
2023-01-20 15:56:58 +0100

    profiles changes
    profile links and profile creation

--- DjangoWebProject/holonet/views.py
+++ DjangoWebProject/holonet/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpRequest
-from .models import Post
+from .models import Post, HolonetUser
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -10,7 +10,7 @@ def index(request):
     assert isinstance(request, HttpRequest)
     post_list = Post.objects.order_by('-create_date')[:10]
     params = {
-        'title':'Holonet',
+        'title':'Home',
         'year':datetime.now().year,
         'post_list':post_list
         }
@@ -18,13 +18,24 @@ def index(request):
 
 def profile(request):
     assert isinstance(request, HttpRequest)
-    username = request.GET['user']
-    userID = User.objects.get_by_natural_key(username)
-    post_list = Post.objects.filter(author=userID).order_by('-create_date')[:10]
-    params = {
-        'title':'Holonet',
+    params = {            
         'year':datetime.now().year,
-        'post_list':post_list,
-        'username':username
         }
-    return render(request,'profile.html',params)
+    if request.user.is_authenticated:
+        username = request.GET['user']
+        userID = User.objects.get_by_natural_key(username)
+        Huser = HolonetUser.objects.get(id = userID.id)
+        post_list = Post.objects.filter(author=userID).order_by('-create_date')[:10]
+        params.update({
+            'title': username,
+            'about' : Huser.about,
+            'post_list': post_list,
+            })
+        return render(request,'profile.html',params)
+    else:
+        params.update({
+            'title': 'Sing in',
+            })
+        return render(request, 'create_profile.html', params)
+
+    
